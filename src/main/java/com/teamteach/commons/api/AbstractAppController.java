68a019220c063fb3f675b7b7ef57fa8878c6abd9
Onkar Parmar <onkar.parmar@gmail.com>
2021-04-14 01:01:14 +0530

    msg non-null now

--- src/main/java/com/teamteach/commons/api/AbstractAppController.java
+++ src/main/java/com/teamteach/commons/api/AbstractAppController.java
@@ -4,4 +4,7 @@ public class AbstractAppController {
     protected ApiResponse sendSuccessResponse(Object data) {
         return ApiResponse.createSuccessResponse(data);
     }
+    protected ApiResponse sendStatusResponseWithMsg(Object data, boolean status, String msg) {
+        return ApiResponse.createStatusResponseWithMsg(data, status, msg);
+    }
 }
