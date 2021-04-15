d4b25ae705d5e869e18e8f0c3e3ac13250b71241
Onkar Parmar <onkar.parmar@gmail.com>
2021-04-15 19:04:05 +0530

    rmq to commons

---
+++ src/main/java/com/teamteach/commons/connectors/rabbit/RabbitMQConfig.java
@@ -0,0 +1,24 @@
+package com.teamteach.commons.connectors.rabbit;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    MappingJackson2MessageConverter consumerJackson2MessageConverter() {
+        return new MappingJackson2MessageConverter();
+    }
+}
---
+++ src/main/java/com/teamteach/commons/connectors/rabbit/core/AbstractDocumentMsg.java
@@ -0,0 +1,8 @@
+package com.teamteach.commons.connectors.rabbit.core;
+
+import lombok.Getter;
+
+@Getter
+public abstract class AbstractDocumentMsg {
+    long timestamp;
+}
