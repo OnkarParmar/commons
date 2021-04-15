6766ad94552bf1bfbb15b27dfb859d025b473724
Subhendu Patra <subhendupatra.cst@gmail.com>
2021-04-15 16:42:11 +0530

    Removed Rabbit MQ

--- src/main/java/com/teamteach/commons/connectors/rabbit/core/AbstractDocumentMsg.java
+++
@@ -1,8 +0,0 @@
-package com.teamteach.commons.connectors.rabbit.core;
-
-import lombok.Getter;
-
-@Getter
-public abstract class AbstractDocumentMsg {
-    long timestamp;
-}
--- src/main/java/com/teamteach/commons/connectors/rabbit/core/XLRTRabbitMessageListenerContainer.java
+++
@@ -1,7 +0,0 @@
-package com.teamteach.commons.connectors.rabbit.core;
-
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-
-public class XLRTRabbitMessageListenerContainer extends SimpleMessageListenerContainer {
-
-}
