00d24a9e90061f4536d24138277b76517a6c4913
EC2 Default User <ec2-user@ip-172-31-13-69.ap-south-1.compute.internal>
2021-04-17 09:52:10 +0530

    api gateway docs

--- src/main/java/com/teamteach/commons/utils/AppSecurityConstants.java
+++ src/main/java/com/teamteach/commons/utils/AppSecurityConstants.java
@@ -1,8 +1,8 @@
 package com.teamteach.commons.utils;
 
 public class AppSecurityConstants {
-    public static final String LOGIN_URL = "/api/login";
-    public static final String SIGNUP_URL = "/api/signup";
+    public static final String LOGIN_URL = "/server/login";
+    public static final String SIGNUP_URL = "/server/signup";
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_HEADER_TOKEN_PREFIX = "Bearer";
 }
